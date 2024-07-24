b6e155c00531e789e5daa8d50e6b6e6c45baeb52
Alexander Hardtke <5yl3kz@googlemail.com>
2024-07-24 21:03:12 +0200

    pokedex CSS done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,19 @@
 const colors = {
-	normal: '#A8A77A',
-	fire: '#EE8130',
-	water: '#6390F0',
-	electric: '#F7D02C',
-	grass: '#7AC74C',
-	ice: '#96D9D6',
-	fighting: '#C22E28',
-	poison: '#A33EA1',
-	ground: '#E2BF65',
-	psychic: '#F95587',
-	bug: '#A6B91A',
-	rock: '#B6A136',
-	ghost: '#735797',
-	dragon: '#6F35FC',
-	fairy: '#D685AD',
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    fairy: '#D685AD',
 };
 
 let index = 0;
@@ -70,9 +70,9 @@ function colorCard(card, i) {
     document.getElementById(`card${card['id']}`).style.backgroundColor = colorCode;
 }
 
-function moreButton() {   
+function moreButton() {
     index += 30;
-    if (index == 120){
+    if (index == 120) {
         document.getElementById('buttonBox').innerHTML = /*html*/`
         `
     }
@@ -81,7 +81,52 @@ function moreButton() {
 }
 
 function openPokedex(i) {
-    showPokedex()
+    showPokedex();
+    fetchPokemon(i);
+}
+
+async function fetchPokemon(i) {
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + i)
+    let pokemon = await response.json();
+    renderPokedexImg(pokemon);
+    colorPokedex(pokemon);
+    renderPokedexName(pokemon);
+    renderPokedexStats(pokemon);
+}
+
+function renderPokedexImg(i) {
+    document.getElementById('screen').innerHTML = /*html*/`
+        <img id="pokeScreen" class="pokeScreen" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i['id']}.png">
+    `
+}
+
+function renderPokedexName(i) {
+    let type1 = i.types[0].type;
+    let type2 = i.types.length > 1 ? i.types[1].type : { name: "" };
+    document.getElementById('greenInfoBox').innerHTML = /*html*/`
+        <p>${i['name']}</p>
+        <p>${type1['name']}</p>
+        <p>${type2['name']}</p>
+    `
+}
+
+function renderPokedexStats(i) {
+    document.getElementById('blackInfoBox').innerHTML = /*html*/`
+    <p>Stats:</p>
+    <p>${i.stats[0].base_stat}</p>
+    <p>${i.stats[1].base_stat}</p>
+    <p>${i.stats[2].base_stat}</p>
+    <p>${i.stats[3].base_stat}</p>
+    <p>${i.stats[4].base_stat}</p>
+    <p>${i.stats[5].base_stat}</p>
+`
+}
+
+function colorPokedex(i) {
+    let type = i.types[0].type;
+    let color = type['name'];
+    let colorCode = colors[color];
+    document.getElementById('screen').style.backgroundColor = colorCode;
 }
 
 function showPokedex() {
@@ -89,6 +134,8 @@ function showPokedex() {
 }
 
 function hidePokedex() {
-    document.getElementById('overlay').classList.remove('showPokeOverlay');
+    if (event.target.id === 'overlay' || event.target.id === 'backExitButton') {
+        document.getElementById('overlay').classList.remove('showPokeOverlay');
+    }
 }
-// beim Öffnen: stats, Entwicklung, nächstes Pokemon, PokedexOG, Suchfunktion, 
+// beim Öffnen: stats, Entwicklung, nächstes Pokemon, Suchfunktion, 
